b21a9422f88ccab38e1cc0552417b084aeb2629f
Wisdom1209 <142598185+Wisdom1209@users.noreply.github.com>
2023-10-29 01:22:53 +0530

    Updates

---
+++ precache-manifest.f9111ac9fae9369d22345d212a276a07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecf7fddb08eac936b81a8fa93f1a324",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "7c06b7ceb61fd9f64173",
+    "url": "/textutils/static/css/main.3e9f990d.chunk.css"
+  },
+  {
+    "revision": "bf5c34c705ad50e3c406",
+    "url": "/textutils/static/js/2.af3fc02c.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/textutils/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "7c06b7ceb61fd9f64173",
+    "url": "/textutils/static/js/main.febf028c.chunk.js"
+  },
+  {
+    "revision": "9eff2318fe253be3dc05",
+    "url": "/textutils/static/js/runtime~main.43961880.js"
+  }
+]);
